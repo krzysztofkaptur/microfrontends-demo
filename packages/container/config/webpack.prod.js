7ba4cb209448695780ff1c82551a53eb57763f08
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-09-22 12:11:12 +0200

    setup container CI

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,29 +1,22 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJSON = require('../package.json')
 
-const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: 'index.html'
-    }
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+        marketing: `marketing@${process.env.PRODUCTION_DOMAIN}/marketing/remoteEntry.js`
       },
       shared: packageJSON.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
     })
   ]
 }
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, prodConfig)
