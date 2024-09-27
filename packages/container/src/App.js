a7839f86e6e566829161eee9b140d9908bd405eb
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-09-27 09:57:44 +0200

    feat(dashboard): Add dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,11 @@
-import React, { lazy, Suspense, useState } from 'react'
-// import MarketingApp from './components/MarketingApp'
-// import AuthApp from './components/AuthApp'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
 import Header from './components/Header'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Router, Redirect, Route, Switch } from 'react-router-dom'
 import {
   StylesProvider,
   createGenerateClassName
 } from '@material-ui/core/styles'
+import { createBrowserHistory } from 'history'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -14,13 +13,22 @@ const generateClassName = createGenerateClassName({
 
 const MarketingApp = lazy(() => import('./components/MarketingApp'))
 const AuthApp = lazy(() => import('./components/AuthApp'))
+const DashboardApp = lazy(() => import('./components/DashboardApp'))
+
+const history = createBrowserHistory()
 
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header
             signedIn={isSignedIn}
@@ -31,11 +39,15 @@ export default () => {
               <Route path="/auth">
                 <AuthApp onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardApp />
+              </Route>
               <Route path="/" component={MarketingApp} />
             </Switch>
           </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   )
 }
